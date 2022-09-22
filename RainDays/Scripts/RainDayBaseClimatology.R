25169b8eac7ebaa1bb27c007aa2d9b11ed2c3863
Yacouba <yacouba.ouedraogo@aims-senegal.org>
2022-09-22 18:20:16 +0100

    RainDayBaseClimatology

---
+++ RainDays/Scripts/RainDayBaseClimatology.R
@@ -0,0 +1,57 @@
+
+
+library(ncdf4)
+library(dplyr)
+library(rio)
+library(ggplot2)
+library(metR)
+library(rgdal)
+rm(list = ls())
+
+Data_Source="CPC-UNIFIED"
+setwd("/Climate-Agriculture-1/RainDays/")
+Africa<-readOGR("/ACMAD_Git/SHP_AFRIQUE/Afrique_frontier_news.shp") 
+MinLon=-25
+MaxLon=55
+MinLat=-40                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+MaxLat=40
+
+
+k=2000
+cpt=7
+A=c("Jan", "Feb", "Mar", "Apr", "May", "Jun" ,"Jul", "Aug", "Sep" ,"Oct", "Nov", "Dec")
+  
+
+
+  for (cpt in 1:12) {
+
+   cpt1=(cpt+1)%%12
+   if(cpt1==0) cpt1=12
+   cpt2=(cpt+2)%%12
+   if(cpt2==0) cpt2=12
+   
+   Season_Name=paste(substr(A[cpt],1,1),substr(A[cpt1],1,1),substr(A[cpt2],1,1),sep = "")
+   Season<-c(A[cpt],A[cpt1],A[cpt2])
+   DDD=data.frame()
+   for (k in 1981:2010) {
+
+  
+  Data=rio::import(paste("/ACMAD_Git/Data/CPC-UNIFIED/DataBase/Number_RainDays/",Season_Name,"/",k,".csv",sep = ""))
+  Data$Year=k
+  DDD=rbind(DDD,Data)
+
+  
+  print(paste("Data/CPC-UNIFIED/DataBase/Number_RainDays/",Season_Name,"/",k,".csv",sep = ""))
+  
+  
+   }
+   
+   NumberDay=DDD%>%
+     group_by(X,Y)%>%
+     summarise(Mean=round(mean(NbRainDay),2),Total=sum(NbRainDay))
+   
+   dir.create(paste("/ACMAD_Git/Data/CPC-UNIFIED/DataBase/Climatology/Number_RainDays",sep = ""),recursive = T,showWarnings = F)
+   rio::export(NumberDay,paste("/ACMAD_Git/Data/CPC-UNIFIED/DataBase/Climatology/Number_RainDays/",Season_Name,".csv",sep = ""))
+
+}
+
